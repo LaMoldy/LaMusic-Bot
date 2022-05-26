cbfe06057656bfa8960aa6e5819d6b3b52d26352
Nikk Jackson <NikkolasJ@Outlook.com>
2022-05-26 15:20:22 -0400

    fix: changed prod tokens for deploy

--- deploy-commands.js
+++ deploy-commands.js
@@ -14,8 +14,8 @@ for (const file of commandFiles) {
   commands.push(command.data.toJSON());
 }
 
-const rest = new REST({ version: 10 }).setToken(process.env.DEV_TOKEN);
+const rest = new REST({ version: 10 }).setToken(process.env.PROD_TOKEN);
 
-rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID_DEV, process.env.GUILD_ID_DEV), { body: commands })
+rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID_PROD, process.env.GUILD_ID_PROD), { body: commands })
   .then(() => console.log('Successfully registered application commands.'))
   .catch(console.error);
